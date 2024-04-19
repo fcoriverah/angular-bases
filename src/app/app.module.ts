66fd5a62ac3b3388e394b684ad84db29ae0ee360
Francisco Rivera Hermosilla <fcoriverah@icloud.com>
2024-04-18 22:41:40 -0400

    Fin de sección 4

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { CounterModule } from './counter/counter.module';
+import { HeroesModule } from './heroes/components/heroes.module';
+
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
+    CounterModule,
     BrowserModule,
+    HeroesModule,
     AppRoutingModule
   ],
   providers: [],
